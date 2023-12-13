67bd34b6442535d2d88497538021294e6f00fa20
tue <46587078+tue132@users.noreply.github.com>
2023-12-13 14:40:27 -0500

    Update align_trim.sh

--- GeneFamily/util/align_trim.sh
+++ GeneFamily/util/align_trim.sh
@@ -2,5 +2,5 @@
 for i in `cat id.txt`
 do 
 mafft --auto ${i}.fa > aligned/${i}.aligned.fa
-trimal -in aligned/${i}.aligned.fa -out aligned/${i}.aligned.trimed.fa -htmlout aligned/${i}.aligned.trimed.fa.html -automated1
+trimal -in aligned/${i}.aligned.fa -out aligned/${i}.aligned.trimmed.fa -htmlout aligned/${i}.aligned.trimmed.fa.html -automated1
 done
