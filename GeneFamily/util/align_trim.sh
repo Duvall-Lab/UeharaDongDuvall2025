9b40f7e1f1ff70447ec132d8bd516a48830c75a7
Takuya Uehara <46587078+tue132@users.noreply.github.com>
2025-06-17 17:26:53 +0900

    Delete GeneFamily directory

--- GeneFamily/util/align_trim.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-for i in `cat id.txt`
-do 
-mafft --auto ${i}.fa > aligned/${i}.aligned.fa
-trimal -in aligned/${i}.aligned.fa -out aligned/${i}.aligned.trimmed.fa -htmlout aligned/${i}.aligned.trimmed.fa.html -automated1
-done
